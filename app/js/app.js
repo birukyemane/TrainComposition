cc44cc6bddd67d88f6c8372039341aa52610b4db
bir_yem <bir_yem@yahoo.com>
2017-08-22 12:30:18 -0400

    - Make the web page dynamic    - create separate modules for app module and controllers    - Add text box to filter the list    - Notes - hard-coded dataset is used to create list of  train numbers    - issues - karma and protractor have error probably because online IDE is used and can not find the binary file for Chrome

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,5 @@
 'use strict';
 
 /* App Module */
+
+angular.module('trainCompositionApp', ['trainCompositionControllers']);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,18 @@
 'use strict';
 
 /* Controllers */
+
+var trainCompositionControllers = angular.module('trainCompositionControllers', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+trainCompositionControllers.controller('TrainCompositionController', function TrainCompositionController($scope) {
+  $scope.trains = [
+    {
+      trainType: 'S',
+      trainNumber: '967'
+    }, {
+      trainType: 'IC',
+      trainNumber: '962'
+    }
+  ];
+});
